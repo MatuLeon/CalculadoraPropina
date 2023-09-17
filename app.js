9816793e65ab24be76f204c88b0b6e7a9ca7efa8
MatuLeon <matiasleon2506@gmail.com>
2023-09-17 11:34:21 -0300

    Funcionabilidad completa, falta agregar efectos de error

--- app.js
+++ app.js
@@ -1,3 +1,88 @@
-let propina = document.getElementById ("btn-Propina")
+
+let valorCuenta = 0
+let comensalesNum = 1
+let propina = 0
+let cuenta = document.getElementById("cuenta");
+let comensales = document.getElementById("comensales");
+let botonPropina = document.querySelectorAll(".btn-Propina");
+let personalizarPropina = document.getElementById("personalizar");
+let propinaPersona = document.getElementById("propinaPersona");
+let cuentaTotal = document.getElementById("cuentaTotal");
+let reset = document.getElementById('reset');
+
+
+
+
+function validaCuenta() {
+    cuenta.addEventListener('input', () => {
+        let validation = /^[0-9,$]*$/
+        if(!validation.test(cuenta.value)){
+            
+        }else{
+            valorCuenta = parseFloat(cuenta.value)
+            totalCuenta()
+        }
+
+    })
+}
+
+function click_Btn() {
+    botonPropina.forEach(btn => {
+        btn.addEventListener('click', porcentajePropina => {
+            propina = Number(porcentajePropina.target.innerText.slice(0, -1))
+            btn.classList.add = 'btn-Propina_select'
+            totalCuenta()
+        })
+    })
+}
+
+
+
+function numComensales() {
+    comensales.addEventListener('input', () => {
+        let validation = /^[0-9,$]*$/
+        if(!validation.test(comensales.value)){
+
+        }else{
+            comensalesNum = parseFloat(comensales.value)
+            totalCuenta()
+        }
+    })
+}
+
+function personalizar_Propina() {
+    personalizarPropina.addEventListener('input', () => {
+        let validation = /^[0-9,$]*$/
+        if(!validation.test(personalizarPropina.value)){
+            
+        }else{
+            propina = parseFloat(personalizarPropina.value)
+            totalCuenta()
+        }
+    })
+}
+
+function reiniciarCuenta() {
+    reset.addEventListener('click', () => {
+        valorCuenta = 0
+        comensalesNum = 1
+        propina = 0
+        cuenta.value = 0
+        comensales.value = 0
+        totalCuenta()
+    })
+}
+
+function totalCuenta() {
+    propinaPersona.innerText = '$' +((valorCuenta * (propina / 100)) / comensalesNum).toFixed(2);
+    cuentaTotal.innerText = '$' +((valorCuenta * (1 + propina / 100)) / comensalesNum).toFixed(2);
+}
+
+validaCuenta();
+click_Btn();
+numComensales();
+personalizar_Propina();
+reiniciarCuenta()
+
 
 
